38b3d84ca35868a6acc2fb5c5d986ff10373d862
morizin <50985248+morizin@users.noreply.github.com>
2020-06-09 22:11:52 +0530

    Add files via upload

---
+++ dqn/exercise/model.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed, fc1 = 64, fc2 = 64):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        "*** YOUR CODE HERE ***"
+        self.fc1 = nn.Linear(state_size, fc1)
+        self.fc2 = nn.Linear(fc1 , fc2)
+        self.fc3 = nn.Linear(fc2 , action_size)
+
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
